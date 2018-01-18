c9292096d5fd32f26484505836024cae5dd52854
Giacomo Tartari <giacomo.tartari@uit.no>
2018-01-18 11:11:35 +0100

    More or less complete test API server, probably needs a but of polishing here and there

--- cmd/nlbtest/main.go
+++ cmd/nlbtest/main.go
@@ -16,7 +16,7 @@ import (
 
 var (
 	frontends = map[string]nlb.Frontend{}
-	//services  map[string]nlb.Services
+	services  = map[string]nlb.Service{}
 )
 
 func main() {
@@ -25,6 +25,12 @@ func main() {
 	router.HandleFunc("/frontends/{name}", listFrontends).Methods("GET")
 	router.HandleFunc("/frontends", newFrontend).Methods("POST")
 	router.HandleFunc("/frontends/{name}", editFrontends).Methods("PUT", "DELETE", "PATCH")
+
+	router.HandleFunc("/services", listServices).Methods("GET")
+	router.HandleFunc("/services/{name}", listServices).Methods("GET")
+	router.HandleFunc("/services", newService).Methods("POST")
+	router.HandleFunc("/services/{name}", editService).Methods("PUT", "DELETE", "PATCH")
+
 	http.ListenAndServe(":8080", router)
 }
 
@@ -67,16 +73,19 @@ func newFrontend(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	frontend := nlb.Frontend{}
+	newFront := nlb.Frontend{}
 	decoder := json.NewDecoder(req.Body)
-	error := decoder.Decode(&frontend)
+	error := decoder.Decode(&newFront)
 	if error != nil {
 		log.Println(error.Error())
 		http.Error(res, error.Error(), http.StatusInternalServerError)
 		return
 	}
-	frontends[frontend.Metadata.Name] = frontend
-	outgoingJSON, err := json.Marshal(frontends)
+	now := time.Now()
+	newFront.Metadata.CreatedAt = now
+	newFront.Metadata.UpdatedAt = now
+	frontends[newFront.Metadata.Name] = newFront
+	outgoingJSON, err := json.Marshal(newFront.Metadata)
 	if err != nil {
 		log.Println(error.Error())
 		http.Error(res, err.Error(), http.StatusInternalServerError)
@@ -118,6 +127,109 @@ func editFrontends(res http.ResponseWriter, req *http.Request) {
 		frontend.Metadata.UpdatedAt = time.Now()
 		frontends[name] = frontend
 		outgoingJSON, err := json.Marshal(frontend)
+		if err != nil {
+			log.Println(error.Error())
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
+		fmt.Fprint(res, string(outgoingJSON))
+	}
+}
+
+func listServices(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(req)
+	name := vars["name"]
+
+	var outData interface{}
+	if len(name) != 0 { //we want a specific frontend
+		var present bool
+		outData, present = services[name]
+		if !present {
+			res.WriteHeader(http.StatusNotFound)
+			//fmt.Fprint(res, string("Frontend not found"))
+			return
+		}
+
+	} else { //if a name is not specified we return all the frontends
+		outData = services
+	}
+	outgoingJSON, error := json.Marshal(outData)
+	if error != nil {
+		log.Println(error.Error())
+		http.Error(res, error.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(res, string(outgoingJSON))
+}
+
+func newService(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(req)
+	name := vars["name"]
+	_, present := services[name]
+	if present {
+		err := errors.Errorf("Frontend %s already present\n", name)
+		log.Println(err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	newSvc := nlb.Service{}
+	decoder := json.NewDecoder(req.Body)
+	error := decoder.Decode(&newSvc)
+	if error != nil {
+		log.Println(error.Error())
+		http.Error(res, error.Error(), http.StatusInternalServerError)
+		return
+	}
+	now := time.Now()
+	newSvc.Metadata.CreatedAt = now
+	newSvc.Metadata.UpdatedAt = now
+	services[newSvc.Metadata.Name] = newSvc
+	outgoingJSON, err := json.Marshal(newSvc.Metadata)
+	if err != nil {
+		log.Println(error.Error())
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	res.WriteHeader(http.StatusCreated)
+	fmt.Fprint(res, string(outgoingJSON))
+}
+
+func editService(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(req)
+	name := vars["name"]
+
+	switch req.Method {
+	case "PUT":
+		delete(services, name)
+		newFrontend(res, req)
+	case "DELETE":
+		delete(services, name)
+		res.WriteHeader(http.StatusNoContent)
+	case "PATCH":
+		editingFront, ok := services[name]
+		if !ok {
+			res.WriteHeader(http.StatusNotFound)
+			//fmt.Fprint(res, string("Frontend not found"))
+			return
+		}
+
+		svc := nlb.Service{}
+		decoder := json.NewDecoder(req.Body)
+		error := decoder.Decode(&svc)
+		if error != nil {
+			log.Println(error.Error())
+			http.Error(res, error.Error(), http.StatusInternalServerError)
+			return
+		}
+		svc.Metadata.CreatedAt = editingFront.Metadata.CreatedAt
+		svc.Metadata.UpdatedAt = time.Now()
+		services[name] = svc
+		outgoingJSON, err := json.Marshal(svc)
 		if err != nil {
 			log.Println(error.Error())
 			http.Error(res, err.Error(), http.StatusInternalServerError)
